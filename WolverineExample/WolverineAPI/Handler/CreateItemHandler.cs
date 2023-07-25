661137d749ace52c95093e6e9bb891bfaaae8877
Vittorio Fava (stage) <vittorio.fava@24consulting.it>
2023-07-25 16:08:59 +0200

    add simple test example

--- WolverineExample/WolverineAPI/Handler/CreateItemHandler.cs
+++ WolverineExample/WolverineAPI/Handler/CreateItemHandler.cs
@@ -8,19 +8,22 @@ namespace WolverineAPI.Handler;
 [Transactional]
 public static class CreateItemHandler
 {
-    public static async Task<ItemCreated> Handle(CreateItemCommand command, ItemDbContext dbContext)
+    public static async Task<ItemCreated> Handle(
+        CreateItemCommand command, 
+        ItemDbContext dbContext)
     {
         Item item = command.Item;
         item.Id = Guid.NewGuid();
 
         dbContext.Items.Add(item);
-
+        
         await Console.Out.WriteLineAsync($@"
 
     ITEM CREATO: {item.Id}
 
 ");
 
-        return new ItemCreated((Guid)item.Id);//.ScheduledAt(DateTimeOffset.Now.AddSeconds(10));
+        return new ItemCreated((Guid)item.Id);
+            //.ScheduledAt(DateTimeOffset.Now.AddSeconds(10));
     }
 }
